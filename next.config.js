2b9ca313eda0f86690da1494e1267a7e23b19ef7
jnfrati <nicofrati@gmail.com>
2022-06-17 22:38:29 -0300

    Setup rewrite for subdomain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  rewrites: [
+    {
+      source: "/:path*",
+      has: [
+        {
+          type: "host",
+          value: ":id.(:host)",
+        },
+      ],
+      destination: "/:id/:path*",
+    },
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
